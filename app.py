86bcd6a123c4a6b2d22a26eb995311ab1a26ed34
Tor Parawell <torparawell@Tors-MacBook-Pro.local>
2023-04-27 10:51:46 -0700

    Attempting to add create budget function

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from db_operations import *
 import db_operations as db
 
+def display_table(conn, tablename):
+    db.pretty_print_table(conn, tablename)
+
+def display_user_table (conn, user_id, tablename):
+    db.display_user_table(conn, user_id, tablename)
+
+def create_a_budget(conn, user_id):
+    pretty_print_table(conn, 'budgets')
+    pretty_print_table(conn, 'categories')
+
+
 # Main
 def main():
     conn = connect_database()
     cursor = conn.cursor()
-    
-    db.pretty_print_table(conn, 'Transactions')
+    print("Welcome to the Credit Application Database")
 
+    # Test
+    display_user_table(conn, 2, 'Transactions')
+    create_a_budget(conn, 2)
+
+    #import_transactions(conn)
     cursor.close()
     conn.close()
 
