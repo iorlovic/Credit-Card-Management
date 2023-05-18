90d87ce00e3dca0bb8a4ec1b47aa7e101a6e0dab
Ivan Orlovic <orlovic@chapman.edu>
2023-05-18 16:25:36 -0700

    working ui ... just need to add data

--- djangoUI/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def login(request):
-    return render(request, 'myapp/login.html')
-
-def dashboard(request):
-    return render(request, 'myapp/dashboard.html')
--- workingUi/app.py
+++ workingUi/app.py
@@ -27,26 +27,30 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
-
-@app.route("/dashboard", methods=["GET", "POST"])
-@login_required
-def dashboard():
     if request.method == "POST":
         user_id = request.form["userid"]
         email = request.form["email"]
     
         if user_id in users and users[user_id]["email"] == email:
             session["user_id"] = user_id
-            return render_template("dashboard.html", user=users[user_id])
+            return redirect(url_for("dashboard"))
         else:
             flash("User not found or incorrect email")
-            return redirect(url_for("login"))
-    else:
-        user_id = session["user_id"]
-        return render_template("dashboard.html", user=users[user_id])
+            return render_template("login.html")
+    return render_template("login.html")
+
+@app.route("/dashboard", methods=["GET", "POST"])
+@login_required
+def dashboard():
+    if request.method == "POST":
+        flash("POST request received")
+        return redirect(url_for("dashboard"))
+
+    user_id = session["user_id"]
+    return render_template("dashboard.html", user=users[user_id])
+
 
 @app.route("/transaction_history")
 @login_required
@@ -62,6 +66,26 @@ def budget_breakdown():
 @login_required
 def spending_by_category():
     return render_template("pichart.html")
+
+@app.route('/logout')
+def logout():
+    # your logout logic here
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+def profile():
+    user = {
+        'username': 'Chuck Norris',
+        'name': 'Chuck Norris',
+        'email': 'user1@example.com',
+        'age': 30
+    }
+    return render_template('profile.html', title='Profile', user=user)
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
